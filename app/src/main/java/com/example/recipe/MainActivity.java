5d7e2f78ce909de70272951226d009e2b076e8b4
tzknkong <tzknkong@gmail.com>
2020-03-31 22:53:22 +0800

    2

--- app/src/main/java/com/example/recipe/MainActivity.java
+++ app/src/main/java/com/example/recipe/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.recipe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] mPlanetTitles;
+    private ListView mDrawerList;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigation_view;
 
     Button b;
 
@@ -22,7 +36,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     b = findViewById(R.id.button1);
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigation_view = (NavigationView) findViewById(R.id.navigation_view);
 
+        // 為navigatin_view設置點擊事件
+        navigation_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                // 點選時收起選單
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                // 取得選項id
+                int id = item.getItemId();
+
+                // 依照id判斷點了哪個項目並做相應事件
+                if (id == R.id.action_home) {
+                    // 按下「首頁」要做的事
+                    Toast.makeText(MainActivity.this, "首頁", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                else if (id == R.id.action_help) {
+                    // 按下「使用說明」要做的事
+                    Toast.makeText(MainActivity.this, "使用說明", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+
+                return false;
+            }
+        });
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
     b.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -32,7 +79,10 @@ public class MainActivity extends AppCompatActivity {
     });
 
 
+
     }
+
+
 
 
 
